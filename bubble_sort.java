6f39548f26c12ae1949445cc4a1a173a62aa0456
Rohit Mathew <rohitjmathew@hotmail.com>
2018-10-04 03:36:04 +0530

    Create bubble_sort.java

---
+++ bubble_sort.java
@@ -0,0 +1,23 @@
+public class bubble_sort {
+    /**
+     * bubble sort , compares adjacent values and swap them if needed,
+     * after every  ith iteration ,ith biggest element would be in place
+     * takes O(n^2) and an inplace algorithm with no extra space.
+     * @param arr
+     * @return
+     */
+    //
+    public int[] bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j + 1] < arr[j]) {
+                    // swap elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
